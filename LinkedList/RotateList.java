3eeac247ca57b3a389e28dd1c11ab6bd4c1b4f2a
shwetavhannure <shwetavhannure20@gmail.com>
2026-03-22 16:58:21 +0530

    Refactor ListNode and RotateList classes

--- LinkedList/RotateList.java
+++ LinkedList/RotateList.java
@@ -2,6 +2,11 @@ class ListNode {
     int val;
     ListNode next;
 
+    ListNode() {
+        this.val = 0;
+        this.next = null;
+    }
+
     ListNode(int val) {
         this.val = val;
         this.next = null;
