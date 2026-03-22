6da4954bf220d681148065364b8c5b0b3b466663
shwetavhannure <shwetavhannure20@gmail.com>
2026-03-22 16:57:59 +0530

    Refactor ListNode class initialization

--- LinkedList/ReverseNodesInKGroup.java
+++ LinkedList/ReverseNodesInKGroup.java
@@ -2,10 +2,14 @@ class ListNode {
     int val;
     ListNode next;
 
-    ListNode() {}
+    ListNode() {
+        this.val = 0;
+        this.next = null;
+    }
 
     ListNode(int val) {
         this.val = val;
+        this.next = null;
     }
 
     ListNode(int val, ListNode next) {
